1c21568c7d3b63edad843e21044586ce72788daf
Kellenerskine <kellenae129@gmail.com>
2021-01-25 18:03:12 -0500

    it works but no user input validation

--- lab1/Input1.java
+++ lab1/Input1.java
@@ -6,14 +6,23 @@ public class Input1 {
     public static void main(String [] args) {
         int num1;
         int num2;
+        int x = -1001;
+        int y = 1001;
 
-        Scanner scnr = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        num1 = scnr.nextInt();
-        num2 = scnr.nextInt();
+        System.out.print("Please enter two numbers separated by spaces (the numbers must be between -1000 and 1000): ");
 
-        System.out.println(num1 + num2);
-        System.out.println(num1 - num2);
-        System.out.println(num1 * num2);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+
+        if (num1 < y && num1 > x && num2 < y && num2 > x/* && num1 != null && num2 != m*/){
+            System.out.println("Sum: " + num1 + " + " + num2 + " = " + (num1 + num2));
+            System.out.println("Difference: " + num1 + " - " + num2 + " = " + (num1 - num2));
+            System.out.println("Product: " + num1 + " x " + num2 + " = " + (num1 * num2));
+        }else{
+            System.out.print("One or more of the numbers you entered were not within the required parameters. Please try again.");
+        }
+        sc.close();
     }
 }
