2454751645473dea6d52f93d56c4331fa3e104ef
Kellenerskine <kellenae129@gmail.com>
2021-01-24 20:22:52 -0500

    rand commit msg\

---
+++ lab1/Initials.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class Initials {
+    public static void main(String[] args) {
+        System.out.println("KK       KK         AAA         EEEEEEEEE");
+        System.out.println("KK     KK          AA AA        EE");
+        System.out.println("KK   KK           AA   AA       EE");
+        System.out.println("KK KK            AAAAAAAAA      EEEEEE");
+        System.out.println("KK   KK         AA       AA     EE");
+        System.out.println("KK     KK      AA         AA    EE");
+        System.out.println("KK       KK   AA           AA   EEEEEEEEE");
+    }
+}
