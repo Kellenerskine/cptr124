741b4048a2151083745c9febdc232adbd0476d25
Kellenerskine <kellenae129@gmail.com>
2021-01-25 11:12:11 -0500

    added the first part of lab1: copy raw code from class repo

---
+++ lab1/ClassCode.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class ClassCode {
+	public static void main(String[] args) {
+		System.out.println("*****************");
+		System.out.println("*               *");
+		System.out.println("*               *");
+		System.out.println("*               *");
+		System.out.println("*               *");
+		System.out.println("*               *");
+		System.out.println("*****************");
+	}
+}
