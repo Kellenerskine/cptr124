c74aea0adf9a59c14f6ac54e0dfa932b34ff2129
Kellenerskine <kellenae129@gmail.com>
2021-01-25 08:47:21 -0500

    a little more detail, still doesnt work though

--- lab1/Input1.java
+++ lab1/Input1.java
@@ -1,9 +1,13 @@
 package lab1;
+
 import java.util.Scanner;
 
 public class Input1 {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         int nums;
         Scanner scnr = new Scanner(System.in);
+        nums = scnr.nextInt();
+        System.out.println("The number you entered is: ")
+        System.out.println(nums);
     }
 }
