99926b6a35c32a8255d0da9ec6366a934e2442c0
Kellenerskine <kellenae129@gmail.com>
2021-01-25 08:38:05 -0500

    basic user input skeleton

---
+++ lab1/Input1.java
@@ -0,0 +1,9 @@
+package lab1;
+import java.util.Scanner;
+
+public class Input1 {
+    public static void main(String[] args) {
+        int nums;
+        Scanner scnr = new Scanner(System.in);
+    }
+}
