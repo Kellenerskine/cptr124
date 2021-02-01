3373d3e733b0f3b1b6837c7793695569499c8d19
Kellenerskine <kellenae129@gmail.com>
2021-02-01 11:51:33 -0500

    S2hms file

---
+++ Lab2/S2hms.java
@@ -0,0 +1,14 @@
+package Lab2;
+import java.util.Scanner;
+
+public class S2hms {
+    public static void main(String[] args){
+        int seconds;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Type in the time in seconds: ");
+        seconds = scn.nextInt();
+
+
+
+    }
+}
