1b038c2139667dfd11f6984b2f4b9bc21a4ce56e
Kellenerskine <kellenae129@gmail.com>
2021-02-02 04:16:19 -0500

    finshed the second program

--- Lab2/S2hms.java
+++ Lab2/S2hms.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 
 public class S2hms {
     public static void main(String[] args){
-        int seconds;
+        int secondsIn;
         Scanner scn = new Scanner(System.in);
         System.out.println("Type in the time in seconds: ");
-        seconds = scn.nextInt();
+        secondsIn = scn.nextInt();
+        int hours = ((secondsIn / 60)/60);
+        int minutes = (((secondsIn - hours)/60) % 60);
+        int seconds = ((secondsIn - (hours * 60 * 60) - (minutes * 60)) % 60);
 
+        System.out.print(hours + " Hours");
+        System.out.print(" " + minutes + " minutes");
+        System.out.print(" " + seconds + " seconds");
 
-
+        scn.close();
     }
 }
