bfc987db8858f1fba2969abd7b27bf91f59e13b8
Kellenerskine <kellenae129@gmail.com>
2021-02-02 05:06:23 -0500

    started on last part of lab, the calorie calculator thingy'

---
+++ Lab2/ExcerComp.java
@@ -0,0 +1,28 @@
+package Lab2;
+import java.util.Scanner;
+
+public class ExcerComp {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int beanBurrito;
+        int salad;
+        int milkshake;
+        int bbCal = 357;
+        int salCal = 185;
+        int milkCal = 388;
+
+        System.out.print("How many bean burritos, bowls of salad, and milkshakes did you consume? ");
+        beanBurrito = scn.nextInt();
+        salad = scn.nextInt();
+        milkshake = scn.nextInt();
+        int totalCal = ((salad * salCal) + (beanBurrito * bbCal) + (milkshake * milkCal));
+
+        System.out.println(totalCal);
+
+
+
+
+        
+    }
+    
+}
