51c20f4dfc65c35b1d9810d1eced2787dc8ddd32
Kellenerskine <kellenae129@gmail.com>
2021-02-08 17:10:02 -0500

    just needs a few small adjustments

--- Lab3/ChangeMaker.java
+++ Lab3/ChangeMaker.java
@@ -6,7 +6,8 @@ public class ChangeMaker {
 
         Scanner scn = new Scanner(System.in);
 
-        double moneyTotal; //less than $10
+        double amount;
+        double moneyTotal;
         int changeDollar; //1 dollar
         int changeQuarters; //25 cents
         int changeDimes; //10 cents
@@ -15,15 +16,28 @@ public class ChangeMaker {
 
         System.out.print("Enter in an amount of money less than $10 and more than $0: ");
         moneyTotal = scn.nextDouble();
+        amount = moneyTotal;
 
-        if(moneyTotal >= 10){
-            System.out.println("The number you entered is higher than $10. Please enter a number less than 10.");
+        if(amount >= 10 || amount < 0){
+            System.out.println("The number you entered is not within the accepted range. 10 > input > 0.");
+        }else{
+            changeDollar = (int)amount / 1;
+            amount = amount - changeDollar * 1;
+            changeQuarters = (int)(amount / 0.25);
+            amount = amount - changeQuarters * 0.25;
+            changeDimes = (int)(amount / 0.10);
+            amount = amount - changeDimes * 0.10;
+            changeNickels = (int)(amount / 0.05);
+            amount = amount - changeNickels * 0.05;
+            changePennies = (int)(amount / 0.01);
+    
+            System.out.println("Your change is: ");
+            System.out.println(changeDollar + " dollars");
+            System.out.println(changeQuarters + " quarters");
+            System.out.println(changeDimes + " dimes");
+            System.out.println(changeNickels + " nickels");
+            System.out.println(changePennies + " pennies");    
         }
-
-        changeDollar = (int)moneyTotal / 1;
-        changeQuarters = (int)moneyTotal / 0.25;
-
-
         scn.close();
     }
 }
