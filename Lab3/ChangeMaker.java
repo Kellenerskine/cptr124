f82fdb342ca39d7acb5fbac168651aefb5a2f83e
Kellenerskine <kellenae129@gmail.com>
2021-02-08 10:50:43 -0500

    something wrong but this is the basis of lab3

---
+++ Lab3/ChangeMaker.java
@@ -0,0 +1,22 @@
+package Lab3;
+import java.util.Scanner
+
+public class ChangeMaker {
+    public static void main(String [] args) {
+        
+        Scanner scn = new Scanner(System.in);
+
+        int moneyTotal; //less than $10
+        int changeNickels; //5 cents
+        int changeDimes; //10 cents
+        int changeQuarters; //25 cents
+        int changePennies; //1 cent
+
+        moneyTotal = scn.nextInt();
+
+        if(moneyTotal > 10){
+            System.out.println("The number you entered is higher than $10. Please enter a number less than 10.");
+        }
+        System.out.println("random");
+    }
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 4;
+        int z = y % x;
+        System.out.println(z);
+    }
+    
+}
