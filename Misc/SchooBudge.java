b820e8cc425dbbdaf825bf84bd4b315331802997
Kellenerskine <kellenae129@gmail.com>
2021-02-19 09:29:59 -0500

    schoolbudge.java

---
+++ Misc/SchooBudge.java
@@ -0,0 +1,41 @@
+package Misc;
+
+import java.util.Scanner;
+
+public class SchooBudge {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        //Monthly income:
+        System.out.print("How many jobs do you have? ");
+        float numJobs = scn.nextFloat();
+        float monthlyPay = 0;
+
+        int cycle = 1;
+        while (cycle <= numJobs) {
+            System.out.println("How much do you make in job " + cycle);
+            float incomePerJob = scn.nextFloat();
+            System.out.println("How many hours a week do you work?");
+            float weeklyHours = scn.nextFloat();
+            float moneyPerWeek = (incomePerJob * weeklyHours);
+            float monthlyIncome = (moneyPerWeek * 4);
+            monthlyPay = monthlyPay + monthlyIncome;
+            System.out.println("you make $" + monthlyIncome + " every month in job " + cycle);
+            cycle++;
+        }
+        System.out.println("You will make $" + monthlyPay + " every month from work.");
+
+
+        //Monthly payment:
+        System.out.print("Enter in the total bill: ");
+        double totalBill = scn.nextInt();
+        System.out.print("Please enter the number of months you will be paying over: ");
+        double numMonths = scn.nextInt();
+        double payPerMonth = (totalBill / numMonths);
+        System.out.println("You need to pay $" + payPerMonth + " every month.");
+
+        double diffPerMonth = (payPerMonth - monthlyPay);
+        System.out.println("You need to make up $" + diffPerMonth + " every month.");
+
+        scn.close();
+    }
+}
