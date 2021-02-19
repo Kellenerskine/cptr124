7df0319bcc0c81bb955caf49ab25897095216da2
Kellenerskine <kellenae129@gmail.com>
2021-02-19 11:09:36 -0500

    more fixes

--- Lab5/GpaCalc.java
+++ Lab5/GpaCalc.java
@@ -9,30 +9,23 @@ public class GpaCalc {
         Scanner scn = new Scanner(System.in);
         char letterGrade = 0;
         
-        HashMap<Character, Double> gradeValue = new HashMap<Character, Double>();
-        gradeValue.put('A', 4.0);
-        gradeValue.put('B', 3.0);
-        gradeValue.put('C', 2.0);
-        gradeValue.put('D', 1.0);
-        gradeValue.put('F', 0.0);
-        Double gradeValueNum = gradeValue.get(letterGrade);
-        
         Double gradeValueSum = 0.0;
-        gradeValueSum = (gradeValueSum + gradeValueNum);
         
         System.out.println(gradeValueSum);
 
-        letterGrade = scn.next().charAt(0);
-
         int count = 1;
         while (letterGrade != 'Z' && letterGrade != 'z'){
+            Double gradeValueNum = gradeValue.get(letterGrade);
+            gradeValueSum = (gradeValueSum + gradeValueNum);
+            HashMap<Character, Double> gradeValue = new HashMap<Character, Double>();
+            gradeValue.put('A', 4.0);
+            gradeValue.put('B', 3.0);
+            gradeValue.put('C', 2.0);
+            gradeValue.put('D', 1.0);
+            gradeValue.put('F', 0.0);
             System.out.println("Input a letter grade as a capital letter. ie. A,B,C,D,F. Type Z to finish and get results.");
             char letter = Character.toUpperCase(letterGrade);
-            String letterAsString = String.valueOf(letter);
-
-            //if (letterAsString != "A" && letterAsString != "B" && letterAsString != "C" && letterAsString != "D" && letterAsString != "F"){}
-            
-
+            letterGrade = scn.next().charAt(0);
             
             System.out.println(letter);
             count++;
