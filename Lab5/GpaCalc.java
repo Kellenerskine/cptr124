a825be7cfede49be09e5258261ff3e0292d1222f
Kellenerskine <kellenae129@gmail.com>
2021-02-19 11:29:26 -0500

    still not working

--- Lab5/GpaCalc.java
+++ Lab5/GpaCalc.java
@@ -12,25 +12,25 @@ public class GpaCalc {
         Double gradeValueSum = 0.0;
         
         System.out.println(gradeValueSum);
-
-        int count = 1;
+        Double gradeValueNum = 0.0;
+        gradeValueSum = (gradeValueSum + gradeValueNum);
+        int count = 0;
         while (letterGrade != 'Z' && letterGrade != 'z'){
-            Double gradeValueNum = gradeValue.get(letterGrade);
-            gradeValueSum = (gradeValueSum + gradeValueNum);
             HashMap<Character, Double> gradeValue = new HashMap<Character, Double>();
             gradeValue.put('A', 4.0);
             gradeValue.put('B', 3.0);
             gradeValue.put('C', 2.0);
             gradeValue.put('D', 1.0);
             gradeValue.put('F', 0.0);
+            //gradeValueNum = gradeValue.get(letterGrade);
             System.out.println("Input a letter grade as a capital letter. ie. A,B,C,D,F. Type Z to finish and get results.");
-            char letter = Character.toUpperCase(letterGrade);
             letterGrade = scn.next().charAt(0);
-            
-            System.out.println(letter);
+            char letter = Character.toUpperCase(letterGrade);
+            System.out.println(gradeValue.get(letter));
             count++;
         }
         Double result = (gradeValueSum / count);
-        System.out.println(result);
+        System.out.println("your result is: " + result);
+        scn.close();
     }
 }
