e3d33bdcc6593abfffdd7c3c1e3b3b4a49e506cf
Kellenerskine <kellenae129@gmail.com>
2021-02-19 10:56:15 -0500

    fixes

--- Lab5/GpaCalc.java
+++ Lab5/GpaCalc.java
@@ -7,29 +7,26 @@ import java.util.HashMap;
 public class GpaCalc {
     public static void main(String [] args){
         Scanner scn = new Scanner(System.in);
-        String letterGrade = scn.next();
+        char letterGrade = 0;
+        
+        HashMap<Character, Double> gradeValue = new HashMap<Character, Double>();
+        gradeValue.put('A', 4.0);
+        gradeValue.put('B', 3.0);
+        gradeValue.put('C', 2.0);
+        gradeValue.put('D', 1.0);
+        gradeValue.put('F', 0.0);
+        Double gradeValueNum = gradeValue.get(letterGrade);
+        
+        Double gradeValueSum = 0.0;
+        gradeValueSum = (gradeValueSum + gradeValueNum);
+        
+        System.out.println(gradeValueSum);
 
-        HashMap<String, Double> gradeValue = new HashMap<String, Double>();
-        gradeValue.put("A", 4.0);
-        gradeValue.put("B", 3.0);
-        gradeValue.put("C", 2.0);
-        gradeValue.put("D", 1.0);
-        gradeValue.put("F", 0.0);
-        Double gradeValueA = gradeValue.get("A");
-        Double gradeValueB = gradeValue.get("B");
-        Double gradeValueC = gradeValue.get("C");
-        Double gradeValueD = gradeValue.get("D");
-        Double gradeValueF = gradeValue.get("F");
-        
-        Double gradeValueSum = (gradeValueA + gradeValueB + gradeValueC + gradeValueD + gradeValueF);
-        
-        System.out.println(gradeValueSum); 
+        letterGrade = scn.next().charAt(0);
 
         int count = 1;
-        while (letterGrade != "Z" || letterGrade != "z"){
-            //char letterGrade;
+        while (letterGrade != 'Z' && letterGrade != 'z'){
             System.out.println("Input a letter grade as a capital letter. ie. A,B,C,D,F. Type Z to finish and get results.");
-            letterGrade = scn.next();//.charAt(0);
             char letter = Character.toUpperCase(letterGrade);
             String letterAsString = String.valueOf(letter);
 
