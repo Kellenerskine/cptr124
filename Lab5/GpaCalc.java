cbb408418c46c7ac58862bcb3851f43826abe56b
Kellenerskine <kellenae129@gmail.com>
2021-02-22 15:29:32 -0500

    finished?

--- Lab5/GpaCalc.java
+++ Lab5/GpaCalc.java
@@ -1,7 +1,5 @@
 package Lab5;
 import java.util.Scanner;
-import java.lang.*;
-import java.util.Map;
 import java.util.HashMap;
 
 public class GpaCalc {
@@ -11,6 +9,8 @@ public class GpaCalc {
         Double gradeValueSum = 0.0;
         Double gradeValueNum = 0.0;
 
+        int passing = 0;
+        int failing = 0;
         int count = 0;
         while (letterGrade != 'Z' && letterGrade != 'z'){
             System.out.print("Input a letter grade as a capital letter. ie. A,B,C,D,F. Type Z to finish and get results. ");
@@ -26,13 +26,20 @@ public class GpaCalc {
             
             if (letterGrade == 'Z' || letterGrade == 'z') {break;}
 
-            System.out.println(gradeValue.get(letter));
+            //System.out.println(gradeValue.get(letter));
             gradeValueNum = gradeValue.get(letter);
             gradeValueSum = (gradeValueSum + gradeValueNum);
+            if (gradeValueNum >= 1.0){passing++;}
+            if (gradeValueNum < 1.0){failing++;}
             count++;
         }
         Double result = (gradeValueSum / count);
-        System.out.println("your result is: " + result);
+        System.out.println(" ");
+        System.out.println("Results: ");
+        System.out.println("Students passing: " + passing);
+        System.out.println("Students failing: " + failing);
+        System.out.println(" ");
+        System.out.printf("Class GPA: " + "%5.3f%n", result);
         scn.close();
     }
 }
