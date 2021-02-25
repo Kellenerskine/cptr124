10372e13bcfb44c28ac5c4a1b01de47374c0d625
Kellenerskine <kellenae129@gmail.com>
2021-02-25 13:54:32 -0500

    just added the even number checker from the asses #1 practice questions

---
+++ Misc/EvenNums.java
@@ -0,0 +1,23 @@
+package Misc;
+import java.util.Scanner;
+
+public class EvenNums {
+    public static void main (String [] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Please enter the list of integers (negative value ends the list): ");
+        int userInput = 0;
+        int evenValsNum = 0;
+        int evenValsSum = 0;
+
+        while (userInput >= 0){
+            userInput = scn.nextInt();
+            if (userInput < 0){break;}
+            if ((userInput % 2) == 0){
+                evenValsNum++;
+                evenValsSum += userInput;
+            }
+        }
+        System.out.println("Number of evens: " + evenValsNum);
+        System.out.println("Sum of evens: " + evenValsSum);
+    }
+}
